330875f169ffa6e8ef383abc0b0ee2d06bde0520
102914174-AG <61397195+102914174-AG@users.noreply.github.com>
2021-04-01 15:46:31 +1100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Nested
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            
+            int counter2;
+
+            System.Console.Write("Please enter a name: ");
+            String input = Console.ReadLine();
+
+            while (counter < 5) {
+                
+                counter2 = counter;
+                while (counter2 < 10) {
+                    Console.WriteLine(input);
+                    counter2 = counter2 + 1;
+
+                } 
+                Console.WriteLine(" ");
+                counter = counter + 1; 
+            }
+            Console.WriteLine("GoodBye");
+            
+        }
+
+        
+    }
+}
